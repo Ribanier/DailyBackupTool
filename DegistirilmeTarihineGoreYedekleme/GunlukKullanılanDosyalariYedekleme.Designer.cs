86c43b3f78bd298f9d318b358ef8686d36d91fad
Sadık YILDIRIM <sadik_yildirim@outlook.com>
2024-12-15 15:56:34 +0300

    Update

---
+++ DegistirilmeTarihineGoreYedekleme/GunlukKullanılanDosyalariYedekleme.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace DegistirilmeTarihineGoreYedekleme
+{
+    partial class GunlukKullanılanDosyalariYedekleme
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sonerisimrb = new System.Windows.Forms.RadioButton();
+            this.sondegistirmerb = new System.Windows.Forms.RadioButton();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(33, 259);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kopyalanacak tarih aralığını seçiniz";
+            // 
+            // sonerisimrb
+            // 
+            this.sonerisimrb.AutoSize = true;
+            this.sonerisimrb.Location = new System.Drawing.Point(276, 299);
+            this.sonerisimrb.Name = "sonerisimrb";
+            this.sonerisimrb.Size = new System.Drawing.Size(98, 17);
+            this.sonerisimrb.TabIndex = 2;
+            this.sonerisimrb.Text = "Son erişim tarihi";
+            this.sonerisimrb.UseVisualStyleBackColor = true;
+            // 
+            // sondegistirmerb
+            // 
+            this.sondegistirmerb.AutoSize = true;
+            this.sondegistirmerb.Checked = true;
+            this.sondegistirmerb.Location = new System.Drawing.Point(276, 259);
+            this.sondegistirmerb.Name = "sondegistirmerb";
+            this.sondegistirmerb.Size = new System.Drawing.Size(123, 17);
+            this.sondegistirmerb.TabIndex = 3;
+            this.sondegistirmerb.TabStop = true;
+            this.sondegistirmerb.Text = "Son değiştirilme tarihi";
+            this.sondegistirmerb.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(33, 299);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Klasör konumunu seçiniz";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(17, 485);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 199);
+            this.listBox1.TabIndex = 9;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(240, 485);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(183, 199);
+            this.listBox2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 460);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Başarılı kopyalamalar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(237, 460);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Başarısız kopyalamalar";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Ayarları kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Durum:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "...";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(427, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Yaptığınız ayarlar seçili alanları işaretlediyseniz eğer bir sonraki çalışmada ka" +
+    "yıtlı gelecektir.";
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(14, 337);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 44);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Programın çalışacağı saati seçiniz";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 353);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(240, 353);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(98, 337);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(56, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Saat(0-24)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(237, 337);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(68, 13);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Dakika(0-60)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 696);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.sondegistirmerb);
+            this.Controls.Add(this.sonerisimrb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton sonerisimrb;
+        private System.Windows.Forms.RadioButton sondegistirmerb;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ DegistirilmeTarihineGoreYedekleme/GunlukKullanılanDosyalariYedekleme.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using DegistirilmeTarihineGoreYedekleme.Properties;
+
+namespace DegistirilmeTarihineGoreYedekleme
+{
+    public partial class GunlukKullanılanDosyalariYedekleme : Form
+    {
+        public GunlukKullanılanDosyalariYedekleme()
+        {
+            InitializeComponent();
+        }
+        string settingsfilepath = "settings.txt";
+        string directorypath = System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        int startmin = 00;
+        int starthour = 10;
+        int sondegisim = 1;
+        int sonerisim = 0;
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!File.Exists(settingsfilepath))
+                {
+                    // Dosya adı ve oluşturulacağı yer
+                    settingsfilepath = "settings.txt";
+
+                    // Dosyaya yazılacak içerik
+                    string[] settingvalues = {directorypath,
+                        startmin.ToString(),
+                        starthour.ToString(),
+                        sondegisim.ToString(),
+                        sonerisim.ToString()
+        };
+
+
+                    // İçeriği dosyaya satır satır yaz
+                    File.WriteAllLines(settingsfilepath, settingvalues);
+                    textBox1.Text = directorypath;
+                    textBox2.Text = starthour.ToString();
+                    textBox3.Text = startmin.ToString();
+                    sonerisimrb.Checked = sonerisim == 1 ? true : false;
+                    sondegistirmerb.Checked = sondegisim == 1 ? true : false;
+
+                }
+                else
+                {
+                    string[] settingvalues = File.ReadAllLines(settingsfilepath);
+                    directorypath = settingvalues[0];
+                    startmin = int.Parse(settingvalues[1]);
+                    starthour = int.Parse(settingvalues[2]);
+                    sondegisim = int.Parse(settingvalues[3]);
+                    sonerisim = int.Parse(settingvalues[4]);
+                }
+                textBox1.Text = directorypath;
+                textBox2.Text = starthour.ToString();
+                textBox3.Text = startmin.ToString();
+                sonerisimrb.Checked = sonerisim == 1 ? true : false;
+                sondegistirmerb.Checked = sondegisim == 1 ? true : false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message);
+            }
+            // Form yüklendiğinde zamanlayıcı başlasın
+            await ScheduleDailyTask(starthour, startmin, ExecuteTask);
+        }
+        private async Task ScheduleDailyTask(int hour, int minute, Action task)
+        {
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                DateTime nextRun = new DateTime(now.Year, now.Month, now.Day, hour, minute, 0);
+
+                // Eğer belirtilen saat geçtiyse, ertesi güne planla
+                if (now > nextRun)
+                {
+                    nextRun = nextRun.AddDays(1);
+                }
+
+                TimeSpan timeToWait = nextRun - now;
+
+                // Kullanıcıya bilgi vermek için Label gibi bir UI bileşeni güncellenebilir
+                UpdateStatusLabel($"Bir sonraki çalıştırma: {nextRun}");
+
+                // Zaman dolana kadar bekle
+                await Task.Delay(timeToWait);
+
+                // Görev çalıştır
+                task.Invoke();
+            }
+        }
+
+        private void ExecuteTask()
+        {
+            // Görevinizi burada çalıştırabilirsiniz
+            MessageBox.Show($"Görev çalıştı: {DateTime.Now}", "Bilgilendirme");
+
+            // Örnek: Bir log yazabilir veya başka bir işlem yapabilirsiniz
+        }
+
+        private void UpdateStatusLabel(string message)
+        {
+            // Bir Label bileşeni varsa durumu göstermek için bu metodu kullanabilirsiniz
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => UpdateStatusLabel(message)));
+            }
+            else
+            {
+                label6.Text = message; // lblStatus, Form üzerindeki bir Label bileşeni
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // Dosya adı ve oluşturulacağı yer
+            try
+            {
+                directorypath = textBox1.Text;
+                starthour = int.Parse(textBox2.Text);
+                startmin = int.Parse(textBox3.Text);
+                sondegisim = sondegistirmerb.Checked ? 1 : 0;
+                sonerisim = sonerisimrb.Checked ? 1 : 0;
+
+                // Dosyaya yazılacak içerik
+                string[] settingvalues = {directorypath,startmin.ToString(), starthour.ToString(), sondegisim.ToString(), sonerisim.ToString()
+        };
+
+
+                // İçeriği dosyaya satır satır yaz
+                File.WriteAllLines(settingsfilepath, settingvalues);
+                DialogResult dialogResult = MessageBox.Show("Program çalıştıktan sonra ayarlarınız aktif edilecektir. \rHemen aktif olması için program yeniden başlatılsın mı?", "Ayarlar Kaydedildi", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bir hata oluştu: " + ex.Message);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+
+            textBox1.Text = dialog.SelectedPath;
+        }
+    }
+}
